c9573fcf7489a22fdbf286b11d1be479ac8756f5
Nathan Yeager <nyeager16@gmail.com>
2025-05-30 18:24:35 -0400

    Change settings for production

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['img.youtube.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
